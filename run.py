f13b397599df497ebc4abfcb08876f1c0c143d0f
Rashidat Adekoya <rawsheedah@yahoo.com>
2022-12-02 20:20:29 +0000

    Create a function for the while true as a display menu function

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 """
  Import required module/library
  """
+# import sys
 from PyDictionary import PyDictionary
+from spellchecker import SpellChecker
+# from art import tprint
 import gspread
 from google.oauth2.service_account import Credentials
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -19,13 +23,30 @@ SHEET = GSPREAD_CLIENT.open('vocabulary_log')
 
 # Create an instance of dictionary from dictionary module
 dictionary = PyDictionary()
+spell = SpellChecker(distance=1)
+# Spread sheet variable
+vocabulary = SHEET.worksheet("vocabulary")
 
-while True:
-    # Responds to user input case sensitivity
-    new_word = input("Enter new word you want to look up here: \n").casefold()
 
-    if new_word == "":
-        break
-     
-    print(dictionary.meaning(new_word))
+def display_menu():
+    """
+    Main display menu
+    """
+    while True:
+        print("""
+        VOCABULARY LOG
+        ==============
+        1. Search, display and log meaning of words
+        2. Search words by letter
+        3. Search random words
+        4. Update worksheet with new word
+        5. Exit Program
+            """)
+        menu_choice = int(input("Select a number of your choice to begin:\n"))
+        if menu_choice == 1:
+            check_word()
+            break
+        print("Invalid number! Enter a number between 1-5: \n")
 
+
+display_menu()
