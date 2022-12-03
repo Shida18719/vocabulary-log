1204b5165fe87526750c4046e5ac2a1c463c8032
Rashidat Adekoya <rawsheedah@yahoo.com>
2022-12-03 18:26:58 +0000

    Modify check word function by adding a spell checker module

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
  Import required module/library
  """
 import sys
+# import random
 from PyDictionary import PyDictionary
 from spellchecker import SpellChecker
 # from art import tprint
@@ -19,27 +20,37 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('vocabulary_log')
+# print(SHEET.readlines())
 
-
-# Create an instance of dictionary from dictionary module
-dictionary = PyDictionary()
-spell = SpellChecker(distance=1)
-# Spread sheet variable
+# Variable for spreadsheet worksheets
 vocabulary = SHEET.worksheet("vocabulary")
+
+
+# Create an instance from  module
+dictionary = PyDictionary()
+spell = SpellChecker()
+
+# Global variable
+word = ""
 
 
 def check_word():
     """
     Take user input and display word meaning
+    Search a word in dictionary and print its coincidences
     Check user misspelt word
     """
+    global word
     word = input("\nEnter new word you want to look up: \n")
+    corrected_word = spell.correction(word)
+    print("corrected_word ", corrected_word)
+
     if word == "":
         sys.exit()
     else:
-        meaning = (dictionary.meaning(word, disable_errors=True))
+        meaning = (dictionary.meaning(corrected_word, disable_errors=True))
         if meaning is None:
-            print("Incorrect word! Please double check it")
+            print("Word not found")
         else:
             try:
                 print("Noun", meaning["Noun"])
@@ -55,20 +66,31 @@ def display_menu():
     Main display menu
     """
     while True:
+
+        # open_page()
         print("""
-        VOCABULARY LOG
-        ==============
+        V O C A B U L A R Y  L O G
+        ==========================
         1. Search, display and log meaning of words
-        2. Search words by letter
-        3. Search random words
-        4. Update worksheet with new word
+        2. Search random words
+        3. Update worksheet with new word
+        4  Display worksheet log
         5. Exit Program
             """)
         menu_choice = int(input("Select a number of your choice to begin:\n"))
         if menu_choice == 1:
             check_word()
+        elif menu_choice == 2:
+            update_worksheet()
+        # elif menu_choice == 3:
+        #     display_log()
+        # elif menu_choice == 4:
             break
-        print("Invalid number! Enter a number between 1-5: \n")
+        else:
+            print("Invalid number! Enter a number between 1-4: \n")
+
+    # except:
+    #     print("Invalid entry")
 
 
 display_menu()
